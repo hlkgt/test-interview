577b9064569cc71be9384114890b7c88c6c8d0d3
hlkgt <haihaoell@gmail.com>
2023-09-21 06:17:05 +0700

    membuat perulangan while

---
+++ while.js
@@ -0,0 +1,6 @@
+let jamMakan = 16;
+let jamSekarang = 1;
+while (jamSekarang < jamMakan) {
+    jamSekarang > jamMakan ? console.log(`belum waktunya makan, karena masih pukul ${jamSekarang}`) : console.log(`belum waktunya makan, karena masih pukul ${jamSekarang}`);
+    jamSekarang++;
+}
